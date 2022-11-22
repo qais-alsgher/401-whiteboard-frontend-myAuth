e70e240407c1e82a0a12b240ab06cb89ee585848
Qais Alsgher <69685164+qais-alsgher@users.noreply.github.com>
2022-11-23 00:40:46 +0300

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../Slicer/authSlicer";
+import postAndCommentsReducer from "../Slicer/postSlicer";
 export default configureStore({
     reducer: {
         auth: authReducer,
-        // post: postReducer,
+        post: postAndCommentsReducer,
     },
 });
 
